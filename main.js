c3a686a4ff54ca908b0a33d67cdaac76238b98fd
Vlad Ionut <ionut_vld@yahoo.com>
2020-04-15 19:24:40 +0300

    Tema 4.2JS

---
+++ main.js
@@ -0,0 +1,22 @@
+for (var i = 1; i <= 2; i++) {
+    for (var j = 1; j <= 2; j++) {
+        console.log(" 0 1 0 1 ");
+        console.log(" 1 0 1 0 ");
+    }
+    break
+}
+
+// var firstNumber = 0,
+//     secondNumber, thirdNumber, fourthNumber;
+// for (var i = 0; i < 4; i++) {
+//     for (var j = 0; j < 4; j++) {
+//         firstNumber == 1 ? secondNumber = firstNumber - 1 : secondNumber = firstNumber + 1;
+//         secondNumber == 1 ? thirdNumber = secondNumber - 1 : thirdNumber = secondNumber + 1;
+//         thirdNumber == 1 ? fourthNumber = thirdNumber - 1 : fourthNumber = thirdNumber + 1;
+//     }
+//     console.log(firstNumber + ' ' + secondNumber + ' ' + thirdNumber + ' ' + fourthNumber)
+//     firstNumber = secondNumber
+//     secondNumber = thirdNumber
+//     thirdNumber = fourthNumber
+//     fourthNumber = firstNumber
+// }
